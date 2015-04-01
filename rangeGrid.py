466984b04c9d81f12cafe83dc1574ed2a84fa330
Alex Walrod <awalrod123@gmail.com>
2015-03-31 22:08:46 -0500

    Fixed path for example file

--- rangeGrid.py
+++ rangeGrid.py
@@ -3,7 +3,7 @@
 from Grid import Grid
 
 def main():
-	g = Grid("/home/alex/Python/Smaller/rangeGrid/example")
+	g = Grid("example")
 	print(g.formatGrid())
 	g.addSource(0,0,5,"Alberto")
 	g.fillRange("Alberto")
